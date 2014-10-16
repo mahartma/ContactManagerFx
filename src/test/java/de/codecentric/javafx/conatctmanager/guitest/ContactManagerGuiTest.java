7a1e1ea1d6773e326788b458918dcdf92d52a998
Max Hartmann <maxadresse@gmail.com>
2014-10-16 11:34:48 +0200

    more tests, ContactManagerGuiTest: gradle bugfix, application.css moved to resources

--- src/test/java/de/codecentric/javafx/conatctmanager/guitest/ContactManagerGuiTest.java
+++ src/test/java/de/codecentric/javafx/conatctmanager/guitest/ContactManagerGuiTest.java
@@ -1,17 +1,19 @@
 package de.codecentric.javafx.conatctmanager.guitest;
 
 import java.awt.AWTException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.jemmy.Point;
 import org.jemmy.fx.AppExecutor;
 import org.jemmy.fx.Browser;
 import org.jemmy.fx.SceneDock;
+import org.jemmy.fx.control.CheckBoxDock;
+import org.jemmy.fx.control.CheckBoxWrap;
 import org.jemmy.fx.control.LabeledDock;
 import org.jemmy.fx.control.TabDock;
 import org.jemmy.fx.control.TabPaneDock;
 import org.jemmy.fx.control.TableViewDock;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.codecentric.javafx.conatctmanager.App;
@@ -20,7 +22,8 @@ import de.codecentric.javafx.conatctmanager.App;
 public class ContactManagerGuiTest {
 	
 	private SceneDock appScene;
-
+	private static AtomicBoolean started = new AtomicBoolean(false);
+	
 	/*
 	 * start the browser to inspect something
 	 */
@@ -29,21 +32,17 @@ public class ContactManagerGuiTest {
 		Browser.runBrowser();
 	}
 	
-	@BeforeClass
-	public static void setupClass() {
-		AppExecutor.executeNoBlock(App.class);
-	}
-	
 	@Before
 	public void setUp() {
+		if (!started.getAndSet(true)) {
+			AppExecutor.executeNoBlock(App.class);
+		}
 		appScene = new SceneDock();
 	}
 	
 	@Test
 	public void shouldAddNewCsontact() throws InterruptedException {
-		
 		new LabeledDock(appScene.asParent(), "addButton").mouse().click();
-		
 		Thread.sleep(2000);
 	}
 
@@ -71,6 +70,26 @@ public class ContactManagerGuiTest {
 		tabDock = new TabDock(tabPaneDock.asTabParent(), "tabContacts");
 		tabPaneDock.selector().select(tabDock.control());		
 	
+		Thread.sleep(2000);
+	}
+
+	@Test
+	public void shouldFilterLogEntries() throws InterruptedException {
+		new LabeledDock(appScene.asParent(), "addButton").mouse().click();
+		new TableViewDock(appScene.asParent()).asTable().select(new Point(0,1));
+		new LabeledDock(appScene.asParent(), "removeButton").mouse().click();
+		
+		TabPaneDock tabPaneDock = new TabPaneDock(appScene.asParent());
+		TabDock tabDock = new TabDock(tabPaneDock.asTabParent(), "tabLog");
+		tabPaneDock.selector().select(tabDock.control());
+		
+		new CheckBoxDock(appScene.asParent(), "modified").selector().select(CheckBoxWrap.State.CHECKED);
+		
+		Thread.sleep(2000);
+		
+		tabDock = new TabDock(tabPaneDock.asTabParent(), "tabContacts");
+		tabPaneDock.selector().select(tabDock.control());		
+		
 		Thread.sleep(2000);
 	}
 }
